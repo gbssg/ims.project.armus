a3ca9abdf6783c564f8d033cfc4d3aa6e4f99272
Jean Guillet <blajean826@gmail.com>
2025-05-19 13:35:36 +0200

    trying to implement ultraschall

---
+++ 2025.05.14 Arduino Roboter v1/include/usDistanceCheck.h
@@ -0,0 +1,11 @@
+#ifndef VARIABLEN_H
+#define VARIABLEN_H
+
+extern int richtungl;
+extern int richtungr;
+
+extern long lastCommandTime;
+
+extern bool driveStateChanged;
+
+#endif
---
+++ 2025.05.14 Arduino Roboter v1/src/usDistanceCheck.cpp
@@ -0,0 +1,7 @@
+int richtungl = 0;
+int richtungr = 0;
+
+bool driveStateChanged = false;
+
+unsigned long lastCommandTime = 0;
+
