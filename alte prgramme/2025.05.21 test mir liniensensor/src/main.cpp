bd8ebbcb4d0fd42bdc59265bad30fdcfd788a728
Jean Guillet <blajean826@gmail.com>
2025-06-05 10:06:49 +0200

    liniensensor funktioiniert nun gut

--- alte prgramme/2025.05.21 test mir liniensensor/src/main.cpp
+++ alte prgramme/2025.05.21 test mir liniensensor/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
-#define liniel 11
-#define liniem 7
-#define linier 8
+#define pin_L 11
+#define pin_M 7
+#define pin_R 8
 
 const int left_ctrl_forward = 2;
 const int right_ctrl_forward = 4;
@@ -10,94 +10,133 @@ const int right_ctrl_forward = 4;
 const int left_pwm = 5;
 const int right_pwm = 6;
 
-const int speedv = 180; // original value: 105 
-const int speedr = 190; //original value 150
+const int speedv = 155; // original value: 105 
+const int speedr = 100; //original value 150
+
+int val_L;
+int val_M;  
+int val_R;
+
+int last_Turn = 0; // 0 = start position, 1 = left, 2 = right
 
 void setup() 
 {
     Serial.begin(9600);
-    pinMode(liniel, INPUT);
-    pinMode(liniem, INPUT);
-    pinMode(linier, INPUT);
+    pinMode(pin_L, INPUT);
+    pinMode(pin_M, INPUT);
+    pinMode(pin_R, INPUT);
     pinMode(left_ctrl_forward, OUTPUT);
     pinMode(right_ctrl_forward, OUTPUT);
     pinMode(left_pwm, OUTPUT);
     pinMode(right_pwm, OUTPUT);
     Serial.println("Setup complete");
+    Serial.print(digitalRead(val_L));
+    Serial.print(digitalRead(val_M));
+    Serial.print(digitalRead(val_R));
+}
+
+void left()
+{
+    digitalWrite(left_ctrl_forward, LOW);
+    analogWrite(left_pwm, speedr);
+
+    digitalWrite(right_ctrl_forward, HIGH);
+    analogWrite(right_pwm, speedv);
+
+    last_Turn = 1;
+}
+
+void right()
+{
+    digitalWrite(left_ctrl_forward, HIGH);
+    analogWrite(left_pwm, speedv);
+
+    digitalWrite(right_ctrl_forward, LOW);
+    analogWrite(right_pwm, speedr);
+    last_Turn = 2;
+}
+
+void front()
+{
+    digitalWrite(left_ctrl_forward, HIGH);
+    analogWrite(left_pwm, speedv);
+
+    digitalWrite(right_ctrl_forward, HIGH);
+    analogWrite(right_pwm, speedv);
+}
+
+void stop()
+{
+    digitalWrite(left_ctrl_forward, LOW);
+    analogWrite(left_pwm, 0);
+
+    digitalWrite(right_ctrl_forward, LOW);
+    analogWrite(right_pwm, 0);
 }
 
 void loop() 
-{
-    Serial.print(digitalRead(liniel));
-    Serial.print(digitalRead(liniem));
-    Serial.print(digitalRead(linier));
+{ 
+    delay(10);
+    val_L = digitalRead(pin_L);
+    val_M = digitalRead(pin_M);
+    val_R = digitalRead(pin_R);
+
+    Serial.print(digitalRead(val_L));
+    Serial.print(digitalRead(val_M));
+    Serial.print(digitalRead(val_R));
     Serial.println();
 
-    if (digitalRead(liniel) + digitalRead(liniem) + digitalRead(linier) == 3)
+    if (val_M == 1)
     {
-        digitalWrite(left_ctrl_forward, HIGH);
-        analogWrite(left_pwm, speedv);
-
-        digitalWrite(right_ctrl_forward, HIGH);
-        analogWrite(right_pwm, speedv);
-    }
-    else if (digitalRead(liniel) + digitalRead(liniem) == 2)
-    {
-        digitalWrite(right_ctrl_forward, HIGH);
-        analogWrite(right_pwm, speedv);
-
-        digitalWrite(left_ctrl_forward, LOW);
-        analogWrite(left_pwm, speedr);
-    }
-    else if (digitalRead(linier) + digitalRead(liniem) == 2)
-    {
-        digitalWrite(right_ctrl_forward, LOW);
-        analogWrite(right_pwm, speedr);
-
-        digitalWrite(left_ctrl_forward, HIGH);
-        analogWrite(left_pwm, speedv);
+        if (val_L == 1 && val_R == 0)
+        {
+            left();
+        }
+        else if(val_L == 0 && val_R == 1)
+        {
+            right();
+        }
+        else
+        {
+            front();
+        }
     }
     else
     {
-        digitalWrite(left_ctrl_forward, LOW);
-        analogWrite(left_pwm, 0);
-
-        digitalWrite(right_ctrl_forward, HIGH);
-        analogWrite(right_pwm, speedv);
+        if (val_L == 1 && val_R == 0)
+        {
+            left();
+        }
+        else if(val_L == 0 && val_R == 1)
+        {
+            right();
+        }
+        else
+        {
+            if (last_Turn == 1)
+            {
+                while (val_L + val_M + val_R < 1)
+                {
+                    val_L = digitalRead(pin_L);
+                    val_M = digitalRead(pin_M);
+                    val_R = digitalRead(pin_R);
+                    left();
+                }
+            }
+            else if (last_Turn == 2)
+            {
+                while (val_L + val_M + val_R < 1)
+                {
+                    val_L = digitalRead(pin_L);
+                    val_M = digitalRead(pin_M);
+                    val_R = digitalRead(pin_R);
+                    right();
+                }
+            }
+            else
+            {
+                stop();
+            }
+        }
     }
-
-
-
-
-
-
 }
-
-    // if (digitalRead(liniel) == 0)
-    // {
-    //     digitalWrite(left_ctrl_forward, HIGH);
-    //     analogWrite(left_pwm, speedv);
-
-    //     digitalWrite(right_ctrl_forward, LOW);
-    //     analogWrite(right_pwm, 0);
-    // }
-    // else if (digitalRead(linier) == 0)
-    // {
-    //     digitalWrite(right_ctrl_forward, HIGH);
-    //     analogWrite(right_pwm, speedv);
-
-    //     digitalWrite(left_ctrl_forward, LOW);
-    //     analogWrite(left_pwm, 0);
-    // }
-    // else
-    // {
-    //     digitalWrite(left_ctrl_forward, LOW);
-    //     analogWrite(left_pwm, 0);
-
-    //     digitalWrite(right_ctrl_forward, LOW);
-    //     analogWrite(right_pwm, 0);
-    // }
-
-
-
-
